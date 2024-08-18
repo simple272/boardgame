7a691534b58988d7f827b94ad7b0e69d51a3c0e1
Deounnal <simple272@gmail.com>
2024-08-18 16:07:49 +0900

    Update service-worker.js

--- lost_cities/service-worker.js
+++ lost_cities/service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'score-calculator-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
